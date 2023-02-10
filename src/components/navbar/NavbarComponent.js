9a04c54212fe4a129cdce88c52858b474e0b75fb
Rifaldo <pn4066283@gmail.com>
2023-02-10 16:46:10 +0700

    navbar first

---
+++ src/components/navbar/NavbarComponent.js
@@ -0,0 +1,186 @@
+import {
+  AppBar,
+  Button,
+  CssBaseline,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import * as React from "react";
+import PropTypes from "prop-types";
+import MenuIcon from "@mui/icons-material/Menu";
+import "./navbar.css";
+import Logo from "./../../assets/images/logo.svg";
+
+const drawerWidth = 240;
+const navItems = ["Home", "About", "Portfolio", "Contact"];
+
+function NavbarComponent(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography
+        variant="h6"
+        sx={{
+          my: 2,
+          alignItems: "center",
+          display: "flex",
+          justifyContent: "center",
+        }}
+        className="logo-text"
+      >
+        <img src={Logo} alt="" className="logo-nav" /> Rundweb
+      </Typography>
+
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar
+          sx={{
+            backgroundColor: "#fff",
+            padding: 2,
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{
+              flexGrow: 1,
+              display: "flex",
+              color: "#000",
+              alignItems: "center",
+            }}
+            className="logo-text"
+          >
+            <img src={Logo} alt="" className="logo-nav" /> Rundweb
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item) => (
+              <Button
+                key={item}
+                sx={{
+                  color: "#2C98FF",
+                  textTransform: "capitalize",
+                  fontSize: 16,
+                }}
+              >
+                {item}
+              </Button>
+            ))}
+          </Box>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="end"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 0.5, display: { sm: "none" }, color: "#2C98FF" }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+        <Typography>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          unde fugit veniam eius, perspiciatis sunt? Corporis qui ducimus
+          quibusdam, aliquam dolore excepturi quae. Distinctio enim at eligendi
+          perferendis in cum quibusdam sed quae, accusantium et aperiam? Quod
+          itaque exercitationem, at ab sequi qui modi delectus quia corrupti
+          alias distinctio nostrum. Minima ex dolor modi inventore sapiente
+          necessitatibus aliquam fuga et. Sed numquam quibusdam at officia
+          sapiente porro maxime corrupti perspiciatis asperiores, exercitationem
+          eius nostrum consequuntur iure aliquam itaque, assumenda et! Quibusdam
+          temporibus beatae doloremque voluptatum doloribus soluta accusamus
+          porro reprehenderit eos inventore facere, fugit, molestiae ab officiis
+          illo voluptates recusandae. Vel dolor nobis eius, ratione atque
+          soluta, aliquam fugit qui iste architecto perspiciatis. Nobis,
+          voluptatem! Cumque, eligendi unde aliquid minus quis sit debitis
+          obcaecati error, delectus quo eius exercitationem tempore. Delectus
+          sapiente, provident corporis dolorum quibusdam aut beatae repellendus
+          est labore quisquam praesentium repudiandae non vel laboriosam quo ab
+          perferendis velit ipsa deleniti modi! Ipsam, illo quod. Nesciunt
+          commodi nihil corrupti cum non fugiat praesentium doloremque
+          architecto laborum aliquid. Quae, maxime recusandae? Eveniet dolore
+          molestiae dicta blanditiis est expedita eius debitis cupiditate porro
+          sed aspernatur quidem, repellat nihil quasi praesentium quia eos,
+          quibusdam provident. Incidunt tempore vel placeat voluptate iure
+          labore, repellendus beatae quia unde est aliquid dolor molestias
+          libero. Reiciendis similique exercitationem consequatur, nobis placeat
+          illo laudantium! Enim perferendis nulla soluta magni error, provident
+          repellat similique cupiditate ipsam, et tempore cumque quod! Qui, iure
+          suscipit tempora unde rerum autem saepe nisi vel cupiditate iusto.
+          Illum, corrupti? Fugiat quidem accusantium nulla. Aliquid inventore
+          commodi reprehenderit rerum reiciendis! Quidem alias repudiandae eaque
+          eveniet cumque nihil aliquam in expedita, impedit quas ipsum nesciunt
+          ipsa ullam consequuntur dignissimos numquam at nisi porro a, quaerat
+          rem repellendus. Voluptates perspiciatis, in pariatur impedit, nam
+          facilis libero dolorem dolores sunt inventore perferendis, aut
+          sapiente modi nesciunt.
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
+
+NavbarComponent.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default NavbarComponent;
